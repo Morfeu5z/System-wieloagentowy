b288f7dd46d4dc23b130b601a59a880d394cdc1c
Aleks <32564717+Morfeu5z@users.noreply.github.com>
2018-05-19 01:54:15 +0200

    Wyodrębnienie js
    * wyciągnięcie js do osobnego pliku
    * dodanie przejścia do sekcji wyświetlania ofert

---
+++ static/source/index.js
@@ -0,0 +1,76 @@
+/**
+ * Lista wskazujaca na zaznaczone przyciski
+ * @type {number[]}
+ */
+ansList = [
+    0, 0, 0,
+    0, 0, 0,
+    0, 0, 0,
+    0, 0, 0
+];
+
+/**
+ * Zmiana listy wartości po kliknięciu
+ * @param butt
+ */
+function buttClick(butt) {
+    var name = butt.name;
+    var elem = document.getElementsByName(name);
+    elem[0].style.backgroundColor = "lightseagreen";
+    elem[1].style.backgroundColor = "lightseagreen";
+    elem[2].style.backgroundColor = "lightseagreen";
+    var x = parseInt(butt.id[1]);
+    var y = parseInt(butt.id[0]);
+    pos = ((y * 3) - 3) + 0;
+    ansList[pos] = false;
+    pos = ((y * 3) - 3) + 1;
+    ansList[pos] = false;
+    pos = ((y * 3) - 3) + 2;
+    ansList[pos] = false;
+    pos = ((y * 3) - 3) + (x - 1);
+    ansList[pos] = true;
+    butt.style.backgroundColor = "limegreen";
+}
+
+/**
+ * Wyślij dane
+ */
+function showAns() {
+    var priList = [
+        document.getElementById('range1').value,
+        document.getElementById('range2').value,
+        document.getElementById('range3').value,
+        document.getElementById('range4').value,
+        document.getElementById('range5').value
+    ];
+    var price = document.getElementById('price').value;
+    console.log('Odpowiedzi: ' + ansList);
+    console.log('Priorytety: ' + priList);
+    console.log('Cena: ' + price);
+    $.post({
+        url: 'getData',
+        data: {
+            'butt': JSON.stringify(ansList),
+            'prio': JSON.stringify(priList),
+            'pric': price,
+        },
+        success: function (response) {
+            console.log('Wysłano agentów.');
+            console.log(response);
+            $('#responde').text(response.response);
+            window.location = '#responde';
+        },
+        error: function (error) {
+            console.log('Coś poszło nie tak.');
+            console.log(error);
+        }
+    });
+}
+
+/**
+ * Update priorytetów
+ * @param range
+ */
+function rangeChange(range) {
+    document.getElementById('span' + range.name).innerText = range.value;
+}
